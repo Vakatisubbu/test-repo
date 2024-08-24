60e76866a623844af55399010b68c54778d168f4
vakati.subbu <vakati.subbu@gmail.com>
2024-08-24 15:14:16 +0530

    test

--- Evenodd.java
+++ Evenodd.java
@@ -8,10 +8,13 @@ public class Evenodd {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             //System.out.println("i = " + i);
+
              if (i % 2 == 0)
              {
                  System.out.println("i = " + i);
              }
+             else  System.out.println("i = " + i);
+
 
         }
     }
