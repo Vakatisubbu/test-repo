bc3c1adfb131d644dd927866363fbb56277b76ce
vakati.subbu <vakati.subbu@gmail.com>
2024-08-24 15:28:34 +0530

    new branch

--- Evenodd.java
+++ Evenodd.java
@@ -4,16 +4,16 @@ public class Evenodd {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 10; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             //System.out.println("i = " + i);
 
              if (i % 2 == 0)
              {
-                 System.out.println("i = " + i);
+                 System.out.println("even: = " + i);
              }
-             else  System.out.println("i = " + i);
+             else  System.out.println("ODD: = " + i);
 
 
         }
