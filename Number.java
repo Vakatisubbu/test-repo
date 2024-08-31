d2a2fb5c143bb4a8d0a25b16ba21462fa5105ad4
vakati.subbu <vakati.subbu@gmail.com>
2024-08-31 11:52:14 +0530

    test

--- Number.java
+++ Number.java
@@ -1,7 +1,7 @@
 public class Number {
     public static void main(String[] args) {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixed
+    // to see how IntelliJ IDEA suggests fixed subbu
     System.out.print("Hello and welcome!");
 
     for (int i = 1; i <= 5; i++) {
