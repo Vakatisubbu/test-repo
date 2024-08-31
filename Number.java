88c015b5ca80862f479f714bbd912a88f124bb6c
vakati.subbu <vakati.subbu@gmail.com>
2024-08-31 12:23:37 +0530

    Test oddeven

--- Number.java
+++ Number.java
@@ -1,7 +1,7 @@
 public class Number {
     public static void main(String[] args) {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixed subbu raja
+    // to see how IntelliJ IDEA suggests fixed subbu raja Number even numbers only
     System.out.print("Hello and welcome!");
 
     for (int i = 1; i <= 5; i++) {
